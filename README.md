cfb24a254cf09c75df75bebba0b7bc0b86b5c6e1
minfaox3 <spdlci30@gmail.com>
2021-12-10 00:21:31 +0900

    [add]bookmark/refactor

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # minbrowser
+## Environment setup for Debian
+1. Install python to execute `main.py`
+    * `sudo apt install python`
+2. Install python3-gi to use GTK
+    * `sudo apt install python3-gi`
+3. Install gir1.2-webkit2-4.0 to use WebKit
+    * `sudo apt install gir1.2-webkit2-4.0`
+
+## Execution method
+`python3 main.py`
+
+## Recommended development tool
+* Glade(https://glade.gnome.org/) for ui editor
+    * `sudo apt install glade`
--- main.py
+++ main.py
@@ -1,94 +1,106 @@
 #!/usr/bin/python 
 # -*- coding: utf-8 -*- 
 
-## Here we imported both Gtk library and the WebKit engine. 
 import gi
-from requests import exceptions
-from requests.sessions import Request
 gi.require_version('Gtk', '3.0')
 gi.require_version('WebKit2', '4.0')
 from gi.repository import Gtk, WebKit2
 
-class Handler: 
-  
+def decode_str(s):
+  for charset in [u'cp932', u'utf-8', u'euc-jp', u'shift-jis', u'iso2022-jp']:
+    try:
+      return s.decode(charset)
+    except:
+      pass
+
+class SignalHandler: 
   def back_button_clicked(self, button):
     webview.go_back()
 
   def reload_button_clicked(self, button):
     webview.reload()
+    search_bar.set_text(uri)
     
   def home_button_clicked(self, button):
-    webview.load_uri("https://www.google.com")
+    webview.load_uri(homepage)
 
   def enterkey_clicked(self, button):
-    webview.load_uri(search_bar.get_text())
+    global search_text
+    search_text = search_bar.get_text()
+    webview.load_uri(search_text)
 
   def source_button_clicked(self, button):
     if(svs.get_property("visible")):
-      sb_image.set_from_stock("gtk-media-forward", Gtk.IconSize.BUTTON)
+      sb_image.set_from_stock("gtk-media-previous", Gtk.IconSize.BUTTON)
       svs.hide()
     else:
       buffer = source_viewer.get_buffer()
-      buffer.set_text(html.encode('utf-8'))
-      sb_image.set_from_stock("gtk-media-previous", Gtk.IconSize.BUTTON)
+      buffer.set_text(html)
+      sb_image.set_from_stock("gtk-media-forward", Gtk.IconSize.BUTTON)
       svs.show()
+
+  def bookmark_button_clicked(self, button):
+    if uri in bookmarks:
+      bookmarks.remove(uri)
+      bookmark_image.set_from_icon_name("non-starred-symbolic", Gtk.IconSize.BUTTON)
+    else:
+      bookmarks.append(uri)
+      bookmark_image.set_from_icon_name("starred-symbolic", Gtk.IconSize.BUTTON)
 
 def load_changed(self, event, ud=None):
   if event == WebKit2.LoadEvent.FINISHED:
-    self.get_main_resource().get_data(None, grdata,None)
+    resource = self.get_main_resource()
+    if resource is not None:
+      resource.get_data(None, grdata,None)
+    else:
+      webview.load_uri("https://www.google.com/search?q="+search_text)
 
 def grdata(self, task, ud=None):
   global html
   html = self.get_data_finish(task)
+  html = decode_str(html)
   if(svs.get_property("visible")):
     buffer = source_viewer.get_buffer()
-    for charset in [u'cp932', u'utf-8', u'euc-jp', u'shift-jis', u'iso2022-jp']:
-        try:
-          html = html.decode(charset)
-          break
-        except:
-          pass
     buffer.set_text(html)
 
 def uri_changed(self, v):
-  search_bar.set_text(self.get_uri())
-
-## Nothing new here.. We just imported the 'ui.glade' file. 
+  global uri
+  uri = self.get_uri()
+  search_bar.set_text(uri)
+  if uri in bookmarks:
+    bookmark_image.set_from_icon_name("starred-symbolic", Gtk.IconSize.BUTTON)
+  else:
+    bookmark_image.set_from_icon_name("non-starred-symbolic", Gtk.IconSize.BUTTON)
+ 
 builder = Gtk.Builder() 
 builder.add_from_file("ui.glade") 
-builder.connect_signals(Handler()) 
+builder.connect_signals(SignalHandler()) 
 
+uri = ""
 html = ""
+search_text = ""
+homepage = "https://www.google.com"
+bookmarks = []
 
 window = builder.get_object("window") 
-
-## Here's the new part.. We created a global object called 'browserholder' which will contain the WebKit rendering engine, and we set it to 'WebKit.WebView()' which is the default thing to do if you want to add a WebKit engine to your program. 
-webview = WebKit2.WebView() 
-
-## To disallow editing the webpage. 
-webview.set_editable(False) 
-
-webview.connect("notify::uri", uri_changed)
-webview.connect("load-changed", load_changed)
-
 search_bar = builder.get_object("search_bar")  
 source_viewer = builder.get_object("source_viewer")
 svs = builder.get_object("svs")
 sb_image = builder.get_object("sb_image")
-
-## The default URL to be loaded, we used the 'load_uri()' method. 
-webview.load_uri("https://www.google.com")
-
-## Here we imported the scrolledwindow1 object from the ui.glade file. 
+reload_image = builder.get_object("reload_image")
+bookmark_image = builder.get_object("bookmark_image")
 viewer = builder.get_object("viewer") 
 
-## We used the '.add()' method to add the 'browserholder' object to the scrolled window, which contains our WebKit browser. 
-viewer.add(webview) 
+webview = WebKit2.WebView() 
+webview.set_editable(False)
 
-## And finally, we showed the 'browserholder' object using the '.show()' method. 
-webview.show() 
+webview.connect("notify::uri", uri_changed)
+webview.connect("load-changed", load_changed)
+webview.load_uri("https://www.google.com")
+
+viewer.add(webview) 
+webview.show()
  
-## Give that developer a cookie ! 
 window.connect("delete-event", Gtk.main_quit) 
 window.show_all() 
 Gtk.main()
