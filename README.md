51c98e30ea06eb7f2a90010414a2dd8e520839f2
minfaox3 <spdlci30@gmail.com>
2021-12-11 22:45:29 +0900

    [add]tab/about dialog

--- README.md
+++ README.md
@@ -1,22 +1,32 @@
 # minbrowser
 ![minbrowser](https://user-images.githubusercontent.com/38807392/145425330-4605376d-52b7-42ae-893c-8c2df7d94e2e.gif)
-
-## Description
 This is a small browser developed as an example of programming with GTK and Python in ChromeOS.  
 By clicking the button on the right of the screen, you can easily view the HTML of the page you are looking at.  
 Other features are equivalent to those of a standard browser.  
 
 ## Environment setup for Debian
-1. Install Python to execute `main.py`
+1. Install Python to execute `main.py`.
     * `sudo apt install python`
-2. Install python3-gi to use GTK
+2. Install python3-gi to use GTK.
     * `sudo apt install python3-gi`
-3. Install gir1.2-webkit2-4.0 to use WebKit
+3. Install gir1.2-webkit2-4.0 to use WebKit.
     * `sudo apt install gir1.2-webkit2-4.0`
 
 ## Execution method
 `python3 main.py`
 
+## Features
+### Added
+* Change homepage
+* Bookmark several webpages.
+* Record history.
+* Change icon theme.
+### Planned
+* Open multiple webpages in one window.
+
 ## Recommended development tool
 * Glade(https://glade.gnome.org/) for ui editor
     * `sudo apt install glade`
+
+## LICENSE
+minbrowser is free and open source software distributed under the [MPL-2.0 License](https://github.com/minfaox3/minbrowser/blob/main/LICENSE).
--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('WebKit2', '4.0')
 from gi.repository import Gtk, WebKit2
+import json
 
+#decode webpage's html to view sourcecode
 def decode_str(s):
   for charset in [u'cp932', u'utf-8', u'euc-jp', u'shift-jis', u'iso2022-jp']:
     try:
@@ -13,94 +15,205 @@ def decode_str(s):
     except:
       pass
 
+class BrowseTab(Gtk.Box):
+  def __init__(self) -> None:
+      super().__init__(homogeneous=Gtk.Orientation.HORIZONTAL, spacing=3)
+      self.view_window = Gtk.ScrolledWindow.new(None, None)
+      self.source_view_window = Gtk.ScrolledWindow.new(None, None)
+      self.source_view_button_icon = Gtk.Image.new_from_icon_name("go-last-symbolic-rtl", Gtk.IconSize.BUTTON)
+      self.source_view_button = Gtk.Button.new()
+      self.source_view = Gtk.TextView.new()
+      self.webview = WebKit2.WebView()
+      self.source_view_button.set_image(self.source_view_button_icon)
+      self.source_view_button.connect("clicked", source_view_button_clicked)
+      self.source_view_window.add(self.source_view)
+      self.pack_start(self.view_window, True, True, 0)
+      self.pack_start(self.source_view_button, False, True, 0)
+      self.pack_start(self.source_view_window, True, True, 0)
+      self.webview.connect("notify::uri", uri_changed)
+      self.webview.connect("notify::title", title_changed)
+      self.webview.connect("load-changed", load_changed)
+      self.webview.connect("create", create)
+      self.webview.load_uri(homepage)
+      self.webview.set_editable(False)
+      self.view_window.add(self.webview)
+      self.source_view_window.hide()
+      self.webview.show()
+
+  def destroy(self):
+    self.webview.destroy()
+    self.view_window.destroy()
+    self.source_view_button.destroy()
+    self.source_view_button_icon.destroy()
+    self.source_view.destroy()
+    self.source_view_window.destroy()
+    self.destroy()
+
+  def hide_source_view_window(self):
+    self.source_view_window.hide()
+
+  def switch_source_view(self):
+    if(self.source_view_window.get_property("visible")):
+      self.source_view_button_icon.set_from_icon_name("go-last-symbolic-rtl", Gtk.IconSize.BUTTON)
+      self.source_view_window.hide()
+    else:
+      self.source_view.get_buffer().set_text(htmls[operate_page])
+      self.source_view_button_icon.set_from_icon_name("go-last-symbolic", Gtk.IconSize.BUTTON)
+      self.source_view_window.show()
+
+  def go_back(self):
+    self.webview.go_back()
+
+  def reload(self):
+    self.webview.reload()
+  
+  def load_uri(self, uri):
+    self.webview.load_uri(uri)
+
+#this handle connect to gobject's signal
 class SignalHandler: 
-  def back_button_clicked(self, button):
-    webview.go_back()
+  #MainWindowSignals
+  def mw_back_button_clicked(self, button):
+    tabs[operate_page].go_back()
 
-  def reload_button_clicked(self, button):
-    webview.reload()
-    search_bar.set_text(uri)
+  def mw_reload_button_clicked(self, button):
+    tabs[operate_page].reload()
+    search_bar.set_text(uris[operate_page])
     
-  def home_button_clicked(self, button):
-    webview.load_uri(homepage)
+  def mw_home_button_clicked(self, button):
+    tabs[operate_page].load_uri(homepage)
 
-  def enterkey_clicked(self, button):
+  def mw_enterkey_clicked(self, button):
     global search_text
     search_text = search_bar.get_text()
-    webview.load_uri(search_text)
+    tabs[operate_page].load_uri(search_text)
 
-  def source_button_clicked(self, button):
-    if(svs.get_property("visible")):
-      sb_image.set_from_stock("gtk-media-previous", Gtk.IconSize.BUTTON)
-      svs.hide()
-    else:
-      buffer = source_viewer.get_buffer()
-      buffer.set_text(html)
-      sb_image.set_from_stock("gtk-media-forward", Gtk.IconSize.BUTTON)
-      svs.show()
-
-  def bookmark_button_clicked(self, button):
-    if uri in bookmarks:
-      bookmarks.remove(uri)
+  def mw_bookmark_button_clicked(self, button):
+    if uris[operate_page] in bookmarks:
+      bookmarks.remove(uris[operate_page])
       bookmark_image.set_from_icon_name("non-starred-symbolic", Gtk.IconSize.BUTTON)
     else:
-      bookmarks.append(uri)
+      bookmarks.append(uris[operate_page])
       bookmark_image.set_from_icon_name("starred-symbolic", Gtk.IconSize.BUTTON)
+  
+  #pvmSignals
+  def pvm_open_button_clicked(self, button):
+    print("SIGNAL DETECTED:OPEN BOOKMARK")
 
+  def pvm_config_button_clicked(self, button):
+    print("SIGNAL DETECTED:OPEN CONFIG")
+  
+  def pvm_about_button_clicked(self, button):
+    about_dialog.show_all()
+  
+  def pvm_quit_button_clicked(self, button):
+    Gtk.main_quit()
+  
+
+  #AboutMenuSignal
+  def about_dialog_delete_event(self, event, ud):
+    about_dialog.hide()
+    return True
+  
+  def ad_quit_button_clicked(self, button):
+    about_dialog.hide()
+
+  #Signals in Tab
+  def t_add_tab_button_clicked(self, button):
+    global uris
+    global htmls
+    global search_bar_texts
+    global tabs
+    global operate_page
+    recent_op = operate_page
+    search_bar_texts[operate_page] = search_bar.get_text()
+    operate_page = note_book.get_n_pages()
+    uris.append("")
+    htmls.append("")
+    search_bar_texts.append("")
+    tabs.append(BrowseTab())
+    note_book.append_page(tabs[len(tabs)-1], Gtk.Label.new("new tab"))
+    note_book.show_all()
+    note_book.do_change_current_page(note_book, note_book.get_n_pages()-(note_book.get_current_page()+1))
+    tabs[recent_op].hide_source_view_window()
+    tabs[note_book.get_current_page()].hide_source_view_window()
+
+def source_view_button_clicked(button):
+  tabs[operate_page].switch_source_view()
+
+
+#these functions connect to webkit::webview
 def load_changed(self, event, ud=None):
+  global tabs
   if event == WebKit2.LoadEvent.FINISHED:
     resource = self.get_main_resource()
     if resource is not None:
-      resource.get_data(None, grdata,None)
+      resource.get_data(None, get_data,None)
     else:
-      webview.load_uri("https://www.google.com/search?q="+search_text)
+      tabs[operate_page].load_uri("https://www.google.com/search?q="+search_text)
 
-def grdata(self, task, ud=None):
-  global html
-  html = self.get_data_finish(task)
-  html = decode_str(html)
-  if(svs.get_property("visible")):
-    buffer = source_viewer.get_buffer()
-    buffer.set_text(html)
+def get_data(self, task, ud=None):
+  global htmls
+  htmls[operate_page] = decode_str(self.get_data_finish(task))
+  if(tabs[operate_page].source_view_window.get_property("visible")):
+    buffer = tabs[operate_page].source_view.get_buffer()
+    buffer.set_text(htmls[operate_page])
 
 def uri_changed(self, v):
-  global uri
-  uri = self.get_uri()
-  search_bar.set_text(uri)
-  if uri in bookmarks:
+  global uris
+  global search_bar_texts
+  uris[operate_page] = self.get_uri()
+  search_bar_texts[operate_page] = uris[operate_page]
+  search_bar.set_text(uris[operate_page])
+  if uris[operate_page] in bookmarks:
     bookmark_image.set_from_icon_name("starred-symbolic", Gtk.IconSize.BUTTON)
   else:
     bookmark_image.set_from_icon_name("non-starred-symbolic", Gtk.IconSize.BUTTON)
- 
-builder = Gtk.Builder() 
-builder.add_from_file("ui.glade") 
-builder.connect_signals(SignalHandler()) 
 
-uri = ""
-html = ""
-search_text = ""
+def title_changed(self, v):
+  note_book.set_tab_label_text(note_book.get_nth_page(operate_page), self.get_title())
+
+def create(self, n):
+  print("SIGNAL DETECTED:CREATE") 
+
+def switch_page(self, page, page_num):
+  global operate_page
+  global search_bar_texts
+  search_bar_texts[operate_page] = search_bar.get_text()
+  operate_page = page_num
+  search_bar.set_text(search_bar_texts[page_num])
+
+#Main logic
+builder = Gtk.Builder()
+builder.add_from_file("ui.glade")
+builder.connect_signals(SignalHandler())
+
+#global variables
 homepage = "https://www.google.com"
+operate_page = 0
 bookmarks = []
+uris = []
+tabs = []
+htmls = []
+search_bar_texts = []
 
-window = builder.get_object("window") 
-search_bar = builder.get_object("search_bar")  
-source_viewer = builder.get_object("source_viewer")
-svs = builder.get_object("svs")
-sb_image = builder.get_object("sb_image")
-reload_image = builder.get_object("reload_image")
-bookmark_image = builder.get_object("bookmark_image")
-viewer = builder.get_object("viewer") 
+#get gobjects
+window = builder.get_object("window")
+about_dialog = builder.get_object("about_dialog")
+search_bar = builder.get_object("mw_search_bar")
+reload_image = builder.get_object("mw_reload_image")
+bookmark_image = builder.get_object("mw_bookmark_image")
+note_book = builder.get_object("mw_note_book")
 
-webview = WebKit2.WebView() 
-webview.set_editable(False)
+note_book.connect("switch-page", switch_page)
 
-webview.connect("notify::uri", uri_changed)
-webview.connect("load-changed", load_changed)
-webview.load_uri("https://www.google.com")
+uris.append("")
+htmls.append("")
+search_bar_texts.append("")
+tabs.append(BrowseTab())
+note_book.append_page(tabs[len(tabs)-1], Gtk.Label.new("new tab"))
 
-viewer.add(webview) 
-webview.show()
- 
-window.connect("delete-event", Gtk.main_quit) 
-window.show_all() 
+window.connect("delete-event", Gtk.main_quit)
+window.show_all()
+tabs[0].hide_source_view_window()
 Gtk.main()
